528b52fd4d9b630d10738635580d1ace185169f4
asib ahmed <ahmedasib1@gmail.com>
2023-11-07 17:22:38 +0600

    login and register form make

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,45 @@
+import { Link } from "react-router-dom";
 
 const Register = () => {
+
+    const handleRegister = event =>{
+        
+    }
+
     return (
-        <div>
-            <p>register</p>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleRegister} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Your Full Name</span>
+                            </label>
+                            <input type="text" placeholder="Fill Your Full Name" name="name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">register</button>
+                        </div>
+                        <Link to='/login' className="label-text-alt link link-hover">Already have an account?</Link>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
